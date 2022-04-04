ef966c01c492b0be094b00fb9160801aa79fa35c
Paula <01160168@pw.edu.pl>
2022-04-04 15:51:28 +0200

    dwie nowe funkcje

--- inf2_proj1.py
+++ inf2_proj1.py
@@ -53,44 +53,6 @@ class Transformacje:
         Z = (N * (1 - self.e2) + h) * math.sin(fi)
         return(X, Y, Z)
         
-    def Vincent(self, fi, lam, fi_k, lam_k):
-        b = self.a * np.sqrt(1 - self.e2)
-        d_lam = lam_k -lam
-        U_A = np.arctan((1 - self.f)*np.tan(fi))
-        U_B = np.arctan((1 - self.f)*np.tan(fi_k))
-        eps = 0.000001/3600 * math.pi/180 # radiany
-        L=d_lam
-     
-        while True:
-            
-            sin_sgm = np.sqrt((np.cos(U_B)*np.sin(L))**2 + (np.cos(U_A)*np.sin(U_B) - np.sin(U_A)*np.cos(U_B)*np.cos(L))**2)
-            cos_sgm = np.sin(U_A)*np.sin(U_B) + np.cos(U_A)*np.cos(U_B)*np.cos(L)
-            sgm = np.arctan(sin_sgm/cos_sgm)
-            
-            sin_alfa = (np.cos(U_A)*np.cos(U_B)*np.sin(L))/sin_sgm
-            cos2_alfa = 1 - sin_alfa**2
-           
-            cos_2sgm_m = cos_sgm - (2*np.sin(U_A)*np.sin(U_B))/cos2_alfa
-          
-            C = (self.f/16)*cos2_alfa*(4 + self.f*(4 - 3*cos2_alfa))
-            
-            L1 = L
-            L= d_lam + (1 - C)*self.f*sin_alfa*(sgm + C*sin_sgm*(cos_2sgm_m + C*cos_sgm*((-1)+2*(cos_2sgm_m)**2)))
-            
-            if np.abs(L1 - L)<eps:
-                break
-        
-        u2 = ((self.a**2 - b**2)/b**2)*cos2_alfa
-        A = 1 + (u2/16384)*(4096 + u2*(-768+u2*(320-175*u2)))
-        B = (u2/1024)*(256 + u2*(-128 + u2*(74-(47*u2))))
-        
-        d_sgm = B*sin_sgm*(cos_2sgm_m + (1/4)*B*(cos_sgm*(-1+2*(cos_2sgm_m**2)) - (1/6)*B*cos_2sgm_m*(-3+4*(sin_sgm**2))*(-3+4*(cos_2sgm_m**2))))
-       
-        s_AB = b*A*(sgm - d_sgm)
-        s_AB = round(s_AB, 3)
-        A_AB = np.arctan((np.cos(U_B)*np.sin(L))/(np.cos(U_A)*np.sin(U_B) - np.sin(U_A)*np.cos(U_B)*np.cos(L)))
-        A_BA = np.arctan((np.cos(U_A)*np.sin(L))/(-np.sin(U_A)*np.cos(U_B) + np.cos(U_A)*np.sin(U_B)*np.cos(L)))+np.pi
-        return s_AB, A_AB, A_BA
     
     def uklad_1992(self, fi, lam):
         m_0 = 0.9993
@@ -157,6 +119,37 @@ class Transformacje:
         
         return x00, y00 
  
+    def xyz2neu(self, X, Y, Z, X0, Y0, Z0):
+        delta_X = X - X0
+        delta_Y = Y - Y0
+        delta_Z = Z - Z0
+            
+        fi, lam, h = self.xyz2blh_hirvonen(X, Y, Z)
+        R = np.array([[-np.sin(fi) * np.cos(lam), -np.sin(lam), np.cos(fi) * np.cos(lam)],
+                         [-np.sin(fi) * np.sin(lam), np.cos(lam), np.cos(fi) * np.sin(lam)],
+                         [np.cos(fi), 0, np.sin(fi)]])     
+        d = np.matrix([delta_X, delta_Y, delta_Z])
+        d = d.T
+        NEU = R * d
+            
+        N = float(NEU[0])
+        E = float(NEU[1])
+        U = float(NEU[2])
+        return(N, E, U)
+ 
+    def az_elev_dis(self, X, Y, Z, X0, Y0, Z0):
+        
+        N, E, U = self.xyz2neu(X, Y, Z, X0, Y0, Z0)
+        
+        HZdist = math.sqrt(E**2 + N**2)
+        ELdist = math.sqrt(N**2 + E**2 + U**2)
+        Az = math.atan2(E, N)
+        elev = math.atan2(U, HZdist)
+        if Az < 0:
+            Az = Az + 2*math.pi
+            
+        return(Az, elev, HZdist, ELdist)
+ 
     
 #TEST
 
@@ -165,13 +158,8 @@ Y = 1409153.590
 Z = 5009571.170
 test = Transformacje("grs80")
 fi, lam, hel = test.xyz2blh_hirvonen(X, Y, Z)
+test.s_m_s(fi)
 X1, Y1, Z1 = test.blh2xyz(fi, lam, hel)
-
-XK = 3664000.500
-YK = 1409000.590
-ZK = 5009000.170
-fi_k, lam_k, hel_k = test.xyz2blh_hirvonen(XK, YK, ZK)
-s, A_AB, A_BA = test.Vincent(fi, lam, fi_k, lam_k)
 
 
 XG = 3763917
@@ -182,3 +170,10 @@ X00, Y00 = test.uklad_2000(fiG, lamG)
 X92, Y92 = test.uklad_1992(fiG, lamG)
 print(X00, Y00)
 print(X92, Y92)
+
+N, E, U = test.xyz2neu(X, Y, Z, X+1, Y+1, Z+1)
+print(N, E, U)
+
+Az, elev, HZdist, ELdist = test.az_elev_dis(X, Y, Z, X+1, Y+1, Z+1)
+test.s_m_s(elev)
+print(HZdist)
